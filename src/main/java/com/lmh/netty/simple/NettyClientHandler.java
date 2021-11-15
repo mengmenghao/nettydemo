52b2951426b5276c82897e20de1409d823a154cb
廖孟豪 <123456>
2021-11-15 23:07:24 +0800

    nettydemo1

---
+++ src/main/java/com/lmh/netty/simple/NettyClientHandler.java
@@ -0,0 +1,37 @@
+package com.lmh.netty.simple;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @program: hntc-mes
+ * @description:
+ * @author: LiaoMengHao
+ * @create: 2021/11/15 10:31
+ **/
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+    // 当通达就绪就会触发
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client" + ctx);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello,server:", CharsetUtil.UTF_8));
+    }
+
+    // 当通道有读取事件时触发
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复的消息:" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器地址：" + ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/lmh/netty/simple/Test.java
@@ -0,0 +1,15 @@
+package com.lmh.netty.simple;
+
+import io.netty.util.NettyRuntime;
+
+/**
+ * @program: hntc-mes
+ * @description:
+ * @author: LiaoMengHao
+ * @create: 2021/11/15 10:40
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(NettyRuntime.availableProcessors());
+    }
+}
